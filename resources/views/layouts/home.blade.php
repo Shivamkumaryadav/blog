b7929486990cdf94d0ad297d46e8889dc69142cd
shivam <shivamyadav4046@gmail.com>
2024-04-15 09:51:32 +0530

    solved links error

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
     <script src="{{ asset('resources/js/app.js') }}"> </script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
-    <script src="{{ asset('resources/js/app.js') }}"> </script>
+    <script src="{{ asset('resources/js/app.js') }}"> </script> --}}
 
-    {{--
+    
     @vite('resources/css/app.css')
-    @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="flex flex-col min-h-screen">
